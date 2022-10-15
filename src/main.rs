e743edceaa70f25d2366f9e9601f43a61f5332d8
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-10-15 15:02:47 +0300

    Split into run and main funcs.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
-use std::env;
-use std::fs;
+use std::{env, fs, process};
 
-const ARGS_EXPECTED_COUNT: usize = 3;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    println!("\nThe {} has been executed...\n" , &args[0]);
-
-    if args.len() < ARGS_EXPECTED_COUNT {
-        println!("Query strings and target file path expected");
-        return;
-    }
-
-    let config = Config::new(&args);
-   
-    println!("Searching for {} in file {}\n", config.query_string, config.file_path);
-
-    let file_contents = match fs::read_to_string(&config.file_path) {
-        Ok(contents) => contents,
-        Err(_) => String::from("File not found.")
-    };
-
-    println!("{}", file_contents)
-}
-
+const QUERY_STRING_INDEX: usize      = 1;
+const TARGET_FILE_PATH_INDEX: usize  = 2;
+const MIN_ARGS_EXPECTED_COUNT: usize = 3;
 
 struct Config{
     query_string: String,
@@ -32,10 +11,33 @@ struct Config{
 }
 
 impl Config {
-    fn new(args: &Vec<String>) -> Self {
-        Config {
-            query_string: args[1].clone(),
-            file_path: args[2].clone()
+    fn build(args: &Vec<String>) -> Result<Self, &'static str> {
+        if args.len() < MIN_ARGS_EXPECTED_COUNT { 
+            return Err("At least query string and target file path expected, \
+            e.g.: ./minigrep Savannah poem.txt")
         }
+        Ok(Config {
+            query_string: args[QUERY_STRING_INDEX].clone(),
+            file_path: args[TARGET_FILE_PATH_INDEX].clone()
+        })
     }
+}
+
+fn run(config: Config) {
+    let file_contents = match fs::read_to_string(&config.file_path) {
+        Ok(contents) => contents,
+        Err(_) => String::from("File not found.")
+    };
+    println!("{}", file_contents)
+}
+
+fn main() {
+    let argv: Vec<String> = env::args().collect();
+
+    let config = Config::build(&argv).unwrap_or_else(|err| {
+        println!("Failed to parse arguments: {}", err);
+        process::exit(1);
+    });
+    
+    run(config);
 }
