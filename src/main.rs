3699c2141f8ca675a3e3996b3f3b66af8793192e
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-10-15 14:07:27 +0300

    Add and impl new for config struct.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,29 @@ fn main() {
         return;
     }
 
-    let query_string = &args[1];
-    let file_path    = &args[2];
+    let config = Config::new(&args);
+   
+    println!("Searching for {} in file {}\n", config.query_string, config.file_path);
 
-    println!("Searching for {} in file {}\n", &query_string, &file_path);
-
-    let file_contents = match fs::read_to_string(&file_path) {
+    let file_contents = match fs::read_to_string(&config.file_path) {
         Ok(contents) => contents,
         Err(_) => String::from("File not found.")
     };
 
     println!("{}", file_contents)
+}
+
+
+struct Config{
+    query_string: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &Vec<String>) -> Self {
+        Config {
+            query_string: args[1].clone(),
+            file_path: args[2].clone()
+        }
+    }
 }
