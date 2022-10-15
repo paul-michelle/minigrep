f06cd7643f0ff118b343f32cea74892abed286d8
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-10-15 12:12:23 +0300

    Collect query string and target file path.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
 
+const ARGS_EXPECTED_COUNT: usize = 3;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args[1]);
+
+    println!("The {} has been executed..." , &args[0]);
+
+    if args.len() < ARGS_EXPECTED_COUNT {
+        println!("Query strings and target file path expected");
+        return;
+    }
+
+    let query_string = &args[1];
+    let file_path    = &args[2];
+
+    println!("Searching for {} in file {}", &query_string, &file_path);
 }
