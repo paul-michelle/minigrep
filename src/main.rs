03be482b5de6da87c16611efac27035223e47d1f
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-10-15 12:31:30 +0300

    Print out file contents or file not found string.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::env;
+use std::fs;
 
 const ARGS_EXPECTED_COUNT: usize = 3;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("The {} has been executed..." , &args[0]);
+    println!("\nThe {} has been executed...\n" , &args[0]);
 
     if args.len() < ARGS_EXPECTED_COUNT {
         println!("Query strings and target file path expected");
@@ -15,5 +16,12 @@ fn main() {
     let query_string = &args[1];
     let file_path    = &args[2];
 
-    println!("Searching for {} in file {}", &query_string, &file_path);
+    println!("Searching for {} in file {}\n", &query_string, &file_path);
+
+    let file_contents = match fs::read_to_string(&file_path) {
+        Ok(contents) => contents,
+        Err(_) => String::from("File not found.")
+    };
+
+    println!("{}", file_contents)
 }
