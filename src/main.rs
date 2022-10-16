817b2c14a668d3b3915b848f986b838f8f1d2c9a
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-10-16 10:36:20 +0300

    Print errors to stderr.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let argv: Vec<String> = env::args().collect();
 
     let config = lib::Config::build(&argv).unwrap_or_else(|err| {
-        println!("Failed to parse arguments: {}", err);
+        eprintln!("Failed to parse arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = lib::run(config) {
-        println!("Failed to read file contents: {}", e);
+        eprintln!("Failed to read file contents: {}", e);
         process::exit(1)
     }
 }
